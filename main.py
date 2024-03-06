fc78b686b8eec882678bcacae889ddb65abcca7c
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-03-06 18:40:26 -0500

    Ejercisio 7 Terminado

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import modules.getempleado as empleado
 import modules.getclientes as clientes
+import modules.getpedidos as pedidos
 #print(empleado.getAllPuestoNombreApellidosEmail(None))
 
-print(empleado.getNombreApellidosPuesto("Representante Ventas"))
+#print(empleado.getNombreApellidosPuesto("Representante Ventas"))
 print()
-
-print(clientes.Nombres("pais"))
+#print(clientes.Nombres("pais"))
+print()
+print(pedidos.getAllDiferentesEstados("estado"))
--- modules/getpedidos.py
+++ modules/getpedidos.py
@@ -1,3 +1,10 @@
 import storage.pedido as pe
 #Devuelve un listado con los distintos estados por los que puede pasar un pedido
-def
+def getAllDiferentesEstados(estado):
+    DiferentesEstados=set()
+    for val in pe.pedido:
+        if val.get("estado") != None:
+            DiferentesEstados.add(val.get("estado"))
+    lista_DiferentesEstados=list(DiferentesEstados)
+    return lista_DiferentesEstados
+
