e3ebf6a5896b618c10b157f276a1714ef818c08c
CarlosJulioGordilloCovaleda <160853185+CarlosJulioGordilloCovaleda@users.noreply.github.com>
2024-03-12 16:02:25 -0500

    Ejercisio

--- modules/getclientes.py
+++ modules/getclientes.py
@@ -1,5 +1,6 @@
 import storage.cliente as cl
 from tabulate import tabulate
+import storage.empleado as em
 # Devuelve un listado con todos los nombres de los clientes españoles
 
 
@@ -12,6 +13,47 @@ def Nombres():
             })
     return Nombres
 
+# Devuelve un listado con todos los clientes que sean de la ciudad de madrid y cuyo representante de venta tenga 
+#El codigo de empleado11 0 30
+
+def getAllCliMadridRepreVen():
+    listaresultado=[]
+    
+    for val in cl.clientes:
+        if val.get("ciudad")=="Madrid":
+            if val.get("codigo_empleado_rep_ventas")==11 or val.get("codigo_empleado_rep_ventas")==30:  
+                listaresultado.append({
+                    "Codigo del cliente":val.get("codigo_cliente"),
+                    "Nombre del cliente":val.get("nombre_cliente"),
+                    "Ciudad":val.get("ciudad"),
+                    "Codigo emp. Rep Ventas":val.get("codigo_empleado_rep_ventas")
+                })
+    return listaresultado
+
+# Obten un listado con el nombre de cada cliente y el nombre y el apellido de su representante de ventas.
+def getAllNombreClientesyNombreyApellidoEmpleados():
+    resultado=[]
+    for val in cl.clientes:
+        resultado.append({
+            "Nombre_Cliente":val.get("nombre_cliente"),
+            "Codigo_Cliente":val.get("codigo_empleado_rep_ventas")
+        })
+        listresultado=[]
+    for cliente in resultado:
+        for val2 in em.empleados:
+            if cliente.get("Codigo_Cliente")==val2.get("codigo_empleado"):
+                listresultado.append({
+                    "Nombre Cliente":cliente.get("Nombre_Cliente"),
+                    "Datos del Representante Venta":(f'{val2.get("nombre")} {val2.get("apellido1")}'),
+                    "Codigo de Empleado":cliente.get("Codigo_Cliente")
+
+                })
+
+    
+    return listresultado
+        
+
+
 
 def menu():
     while True:
@@ -20,6 +62,9 @@ def menu():
                 Reporte de los clientes
             0. Atras
             1. Obtener los nombres de los clientes Españoles
+            2.listado con todos los clientes que sean de la ciudad de madrid
+              y cuyo representante de venta tenga el codigo de empleado 11 0 30
+            3. Ensayo
             
 
     """)
@@ -28,5 +73,13 @@ def menu():
         if opcion == 1:
             print()
             print(tabulate(Nombres(), headers="keys", tablefmt="github"))
+        elif opcion == 2:
+            print()
+            print(tabulate(getAllCliMadridRepreVen(), headers="keys", tablefmt="github"))
+        elif opcion ==3:
+            print(tabulate(getAllNombreClientesyNombreyApellidoEmpleados(),headers="keys", tablefmt="github"))
         elif opcion==0:
             break
+
+
+
